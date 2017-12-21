106006099c6d7202bc9f1a84398adf7c5fbced06
lauret_s <sebastien.lauret@epitech.eu>
2017-12-21 20:00:30 +0100

    update wallet service begin transfert function

--- src/services/application/wallet-service.js
+++ src/services/application/wallet-service.js
@@ -1,20 +1,47 @@
 
 const orm = require('../../common/orm');
 const ms = require('../../common/modelService');
-const bitGo = require('../../common/BitgoAPI');
+const bitgo = require('../../common/bitGoAPI');
 
 const logger = require('../../common/logger');
 
 
-exports.transfert = (body, user, res) => {
-  bitgo.loginWithToken("v2xfa9b3ec7f9b2a8b957df748d63e2d3ea85674eba5237c11d3d1cdc929d3a6db2");
-  bitgo.getWallet("").then(function(wallet) {
-    bitgo.makeExchange(wallet, body.balance, body.addrDest);
-  });
+exports.transfert = (body, clientToken, res) => {
+
+  orm.find(ms.WALLET_AUTH.model, res, {
+    where : {
+      ID_WALLET: 1
+    }
+  }).then(function(walletAuth) {
+    //"v2xfa9b3ec7f9b2a8b957df748d63e2d3ea85674eba5237c11d3d1cdc929d3a6db2"
+    console.log(walletAuth.TOKEN);
+
+    bitgo.loginWithToken(walletAuth.TOKEN);
+    bitgo.getWalletList().then(function(wallets) {
+    //  for (var index in wallets.wallets) {
+//    console.log(wallets);
+      var wallet = wallets.wallets;
+      console.log(wallet[2]);
+      //}
+    });
+     bitgo.getWallet("5a2aaf339a42a23b073150a384ce055b").then(function(wallet) {
+       bitgo.makeExchange(wallet, 6*1e8, "39ajYQvyKwB6ZV1qmC7ceaxVG59FWs7zP5");
+    });
+
+
+  })
+
   return
 }
 
 exports.updateKeyAuth = (body, res) => {
+  orm.transaction(ms.WALLET_AUTH.model, res, function(t) {
+    return orm.update(ms.WALLET_AUTH.model, body, res, {
+      where: {
+        'ID': 1
+      },
+      transaction: t
+    });
+  });
 
-  return
 }
