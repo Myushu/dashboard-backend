feb9f18d69f33cf1729663878e31257012bd4453
lauret_s <sebastien.lauret@epitech.eu>
2017-12-21 21:17:05 +0100

    src/common/errorMap.js

--- src/services/application/wallet-service.js
+++ src/services/application/wallet-service.js
@@ -2,39 +2,39 @@
 const orm = require('../../common/orm');
 const ms = require('../../common/modelService');
 const bitgo = require('../../common/bitGoAPI');
-
+const errorManager = require('../../common/errors');
 const logger = require('../../common/logger');
 
 
 exports.transfert = (body, clientToken, res) => {
+  if (body.ADDR === undefined)
+    return errorManager.handle({name: "addrMissing"}, res);
+  else if (body.BALANCE === undefined)
+    return errorManager.handle({name: "balanceMissing"}, res);
 
-  return orm.find(ms.WALLET_AUTH.model, undefined, {
-    where : {
-      ID_WALLET: 1
-    }
-  }).then(function(walletAuth) {
-    //"v2xfa9b3ec7f9b2a8b957df748d63e2d3ea85674eba5237c11d3d1cdc929d3a6db2"
+  return orm.find(ms.WALLET_AUTH.model, undefined, {where : {ID_WALLET : 1} }).then(function(walletAuth) {
     console.log(walletAuth.TOKEN);
-
-    bitgo.loginWithToken(walletAuth.TOKEN);
-    return setTimeout(function() {
-      res.end('{"status":"error"}');
-    }, 1500);
-     return bitgo.getWallet("5a2aaf339a42a23b073150a384ce055b").then(function(wallet) {
-       return bitgo.makeExchange(wallet, 6*1e8, "39ajYQvyKwB6ZV1qmC7ceaxVG59FWs7zP5").then(function(t){
-          res.end('{"status":"success"}');
-       });
+  //  orm.find(ms.ADMIN_CLIENT.model, undefined, {where : {ID_CLIENT: clientToken}}).then(function(client) {
+    //  console.log(client);
+      bitgo.loginWithToken(walletAuth.TOKEN);
+      return setTimeout(function() {
+        return errorManager.handle({name: "bitgoNotFound"}, res);
+      }, 1500);
+       return bitgo.getWallet("5a2aaf339a42a23b073150a384ce055b").then(function(wallet) {
+         return bitgo.makeExchange(wallet, body.BALANCE * 1e8, body.ADDR).then(function(t){
+            res.end('{"status":"success"}');
+         });
+  //    });
     });
-  })
-
-  return
+   });
 }
 
 exports.updateKeyAuth = (body, res) => {
-  return orm.transaction(ms.WALLET_AUTH.model, undefined, function(t) {
+    if (body.TOKEN === undefined)
+      return errorManager.handle({name: "tokenMissing"}, res);
     return orm.update(ms.WALLET_AUTH.model, body, undefined, {
       where: {
-        'ID': 1
+        'ID_WALLET': 1
       },
       transaction: t
     });
